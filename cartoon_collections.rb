191b96b98662ac80c0871b4a9733dee54ee624f3
Nahum <losond8@gmail.com>
2021-07-12 10:38:41 -0400

    Working with Booleans and Search Enumberables.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index| 
+    puts "#{index + 1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map! do |word| word.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  # array.each do |count| 
+  #   if count.length > 4 
+  #     return true
+  # end
+
+  # array.each do |count| 
+  #   if count.length <= 4 
+  #     return false
+  #   end
+  # end
+
+  array.any? { |call| call.length > 4 }
+
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array.find{|cheese | cheese_types.include?(cheese)}
 end
+
+# roll_call_dwarves(dwarves)
